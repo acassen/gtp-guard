98a731eaf49de5f0cc1c19b19fb25600701170d8
Alexandre Cassen <acassen@gmail.com>
2023-11-20 16:54:40 +0100

    gtp-guard 1.0.3

--- lib/config.h
+++ lib/config.h
@@ -25,7 +25,7 @@
 #define LOG_FACILITY_MAX        7
 #define PROG                    "gtp-guard"
 #define PROG_PID_FILE		"/var/run/gtp-guard.pid"
-#define VERSION_STRING PROG	" v1.0.3-pre2 (2023/11/06)"
+#define VERSION_STRING PROG	" v1.0.3 (2023/11/20)"
 #define COPYRIGHT_STRING	"Copyright (C) 2023 Alexandre Cassen, <acassen@gmail.com>"
 
 
