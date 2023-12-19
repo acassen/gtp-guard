f7ff450c32b9a126ce21c0323b8e28fa61d8e255
Alexandre Cassen <acassen@gmail.com>
2023-12-19 20:50:44 +0100

    router: Initial eBPF routing skeleton

---
+++ src/bpf/gtp_route.c
@@ -0,0 +1,165 @@
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+/*
+ * Soft:        The main goal of gtp-guard is to provide robust and secure
+ *              extensions to GTP protocol (GPRS Tunneling Procol). GTP is
+ *              widely used for data-plane in mobile core-network. gtp-guard
+ *              implements a set of 3 main frameworks:
+ *              A Proxy feature for data-plane tweaking, a Routing facility
+ *              to inter-connect and a Firewall feature for filtering,
+ *              rewriting and redirecting.
+ *
+ * Authors:     Alexandre Cassen, <acassen@gmail.com>
+ *
+ *              This program is free software; you can redistribute it and/or
+ *              modify it under the terms of the GNU Affero General Public
+ *              License Version 3.0 as published by the Free Software Foundation;
+ *              either version 3.0 of the License, or (at your option) any later
+ *              version.
+ *
+ * Copyright (C) 2023 Alexandre Cassen, <acassen@gmail.com>
+ */
+
+#define KBUILD_MODNAME "gtp_fwd"
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/if_vlan.h>
+#include <linux/types.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <sys/socket.h>
+#include <uapi/linux/bpf.h>
+#include <bpf_endian.h>
+#include <bpf_helpers.h>
+#include "gtp_bpf_utils.h"
+#include "gtp.h"
+
+/*
+ *	MAPs
+ */
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__uint(max_entries, 1000000);
+	__type(key, __be32);				/* TEID */
+	__type(value, struct gtp_rt_rule);		/* GTP Encapsulation Rule */
+} teid_egress SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__uint(max_entries, 1000000);
+	__type(key, struct ip_rt_key);			/* ipaddr + tunnelid */
+	__type(value, struct gtp_rt_rule);		/* GTP Encapsulation Rule */
+} teid_ingress SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+	__uint(max_entries, MAX_IPTNL_ENTRIES);
+	__type(key, __be32);
+	__type(value, struct gtp_iptnl_rule);
+} iptnl_info SEC(".maps");
+
+
+/* Packet rewrite */
+static __always_inline void swap_src_dst_mac(struct ethhdr *eth)
+{
+	__u8 h_tmp[ETH_ALEN];
+	__builtin_memcpy(h_tmp, eth->h_source, ETH_ALEN);
+	__builtin_memcpy(eth->h_source, eth->h_dest, ETH_ALEN);
+	__builtin_memcpy(eth->h_dest, h_tmp, ETH_ALEN);
+}
+
+#if 0
+static __always_inline void
+set_ethhdr(struct ethhdr *new_eth, const struct ethhdr *old_eth, __be16 h_proto)
+{
+	__builtin_memcpy(new_eth->h_source, old_eth->h_dest, ETH_ALEN);
+	__builtin_memcpy(new_eth->h_dest, old_eth->h_source, ETH_ALEN);
+	new_eth->h_proto = h_proto;
+}
+#endif
+
+static __always_inline __u16 csum_fold_helper(__u32 csum)
+{
+	__u32 sum;
+	sum = (csum>>16) + (csum & 0xffff);
+	sum += (sum>>16);
+	return ~sum;
+}
+
+static __always_inline void ipv4_csum(void *data_start, int data_size, __u32 *csum)
+{
+	*csum = bpf_csum_diff(0, 0, data_start, data_size, *csum);
+	*csum = csum_fold_helper(*csum);
+}
+
+
+/*
+ *	GTP-ROUTE traffic selector
+ */
+static __always_inline int
+gtp_route_traffic_selector(struct parse_pkt *pkt)
+{
+	return XDP_PASS;
+}
+
+
+/* Ethernet frame parsing and sanitize */
+static __always_inline bool
+parse_eth_frame(struct parse_pkt *pkt)
+{
+	void *data_end = (void *) (long) pkt->ctx->data_end;
+	void *data = (void *) (long) pkt->ctx->data;
+	struct _vlan_hdr *vlan_hdr;
+	struct ethhdr *eth = data;
+	__u16 eth_type, vlan = 0;
+	__u8 offset;
+
+	offset = sizeof(*eth);
+
+	/* Make sure packet is large enough for parsing eth */
+	if ((void *) eth + offset > data_end)
+		return false;
+
+	eth_type = eth->h_proto;
+
+	/* Handle outer VLAN tag */
+	if (eth_type == bpf_htons(ETH_P_8021Q) ||
+	    eth_type == bpf_htons(ETH_P_8021AD)) {
+		vlan_hdr = (void *) eth + offset;
+		vlan = bpf_ntohs(vlan_hdr->hvlan_TCI);
+		pkt->vlan_id = vlan & 0x0fff;
+		offset += sizeof (*vlan_hdr);
+		if ((void *) eth + offset > data_end)
+			return false;
+
+		eth_type = vlan_hdr->h_vlan_encapsulated_proto;
+		vlan_hdr->hvlan_TCI = bpf_htons(pkt->vlan_id);
+	}
+
+	pkt->l3_proto = bpf_ntohs(eth_type);
+	pkt->l3_offset = offset;
+	return true;
+}
+
+SEC("xdp")
+int xdp_route(struct xdp_md *ctx)
+{
+	struct parse_pkt pkt = { .ctx = ctx,
+				 .vlan_id = 0,
+				 .l3_proto = 0,
+				 .l3_offset = 0
+			       };
+
+	if (!parse_eth_frame(&pkt))
+		return XDP_PASS;
+
+	return gtp_route_traffic_selector(&pkt);
+}
+
+char _license[] SEC("license") = "GPL";
