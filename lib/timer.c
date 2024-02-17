acassen/gtp-guard

commit 9fbaf5489805294c906b8b930f1e2175b7f06320

timer: fix timer_node_add
If timer is pending then reset timer to new expiration

lib/timer.c
@@ -253,32 +253,40 @@ timer_node_init(timer_node_t *t_node, int (*fn) (void *), void *arg)
 	timer_node_clear(t_node);
 }
 
-void
-timer_node_add(timer_thread_t *t, timer_node_t *t_node, int sec)
-{
-	pthread_mutex_lock(&t->timer_mutex);
-	t_node->sands = timer_add_now_sec(t_node->sands, sec);
-	rb_add_cached(&t_node->n, &t->timer, timer_node_timer_less);
-	pthread_mutex_unlock(&t->timer_mutex);
-}
-
 int
 timer_node_pending(timer_node_t *t_node)
 {
 	return timerisset(&t_node->sands);
 }
 
+static int
+__timer_node_del(timer_thread_t *t, timer_node_t *t_node)
+{
+	rb_erase_cached(&t_node->n, &t->timer);
+	timer_node_clear(t_node);
+}
+
 int
 timer_node_del(timer_thread_t *t, timer_node_t *t_node)
 {
-	if (!timerisset(&t_node->sands))
+	if (!timer_node_pending(t_node))
 		return -1;
 
 	pthread_mutex_lock(&t->timer_mutex);
-	rb_erase_cached(&t_node->n, &t->timer);
+	__timer_node_del(t, t_node);
 	pthread_mutex_unlock(&t->timer_mutex);
-	timer_node_clear(t_node);
 	return 0;
+}
+
+void
+timer_node_add(timer_thread_t *t, timer_node_t *t_node, int sec)
+{
+	pthread_mutex_lock(&t->timer_mutex);
+	if (timer_node_pending(t_node))
+		__timer_node_del(t, t_node);
+	t_node->sands = timer_add_now_sec(t_node->sands, sec);
+	rb_add_cached(&t_node->n, &t->timer, timer_node_timer_less);
+	pthread_mutex_unlock(&t->timer_mutex);
 }
 
 static void

src/gtp_data.c
@@ -52,7 +52,6 @@ gtp_mirror_rule_get(const struct sockaddr_storage *addr, uint8_t protocol, int i
 	return NULL;
 }
 
-
 gtp_mirror_rule_t *
 gtp_mirror_rule_add(const struct sockaddr_storage *addr, uint8_t protocol, int ifindex)
 {
@@ -128,7 +127,6 @@ gtp_mirror_vty(vty_t *vty)
 }
 
 
-
 /*
  *	Daemon Control Block helpers
  */
@@ -144,7 +142,6 @@ alloc_daemon_data(void)
 	INIT_LIST_HEAD(&new->gtp_apn);
 	INIT_LIST_HEAD(&new->gtp_switch_ctx);
 	INIT_LIST_HEAD(&new->gtp_router_ctx);
-
 
 	return new;
 }

src/gtp_pppoe.c
@@ -414,10 +414,10 @@ static int
 __gtp_pppoe_release(gtp_pppoe_t *pppoe)
 {
 	__set_bit(PPPOE_FL_STOPPING_BIT, &pppoe->flags);
+	pthread_join(pppoe->task, NULL);
 	gtp_pppoe_timer_destroy(pppoe);
 	gtp_pppoe_worker_destroy(pppoe);
 	gtp_ppp_destroy(pppoe);
-	pthread_join(pppoe->task, NULL);
 	close(pppoe->fd_disc);
 	close(pppoe->fd_session);
 	list_head_del(&pppoe->next);

src/gtp_pppoe_proto.c
@@ -207,7 +207,7 @@ int
 pppoe_connect(spppoe_t *s)
 {
 	gtp_pppoe_t *pppoe = s->pppoe;
-	int err;
+	int err, retry_wait = 2;
 
 	if (s->state != PPPOE_STATE_INITIAL)
 		return -1;
@@ -222,7 +222,9 @@ pppoe_connect(spppoe_t *s)
 	}
 
 	/* register timer */
-	timer_node_add(&pppoe->session_timer, &s->t_node, PPPOE_DISC_TIMEOUT);
+	if (!__test_bit(PPPOE_FL_PADI_FAST_RETRY_BIT, &pppoe->flags))
+		retry_wait = PPPOE_DISC_TIMEOUT;
+	timer_node_add(&pppoe->session_timer, &s->t_node, retry_wait);
 	return 0;
 }
 
@@ -292,7 +294,8 @@ pppoe_timeout(void *arg)
 				PPPDEBUG(("%s: pppoe hunique:0x%.8x failed to send PADI\n",
 					 pppoe->ifname, s->unique));
 			}
-			retry_wait = PPPOE_DISC_TIMEOUT * (1 + s->padi_retried);
+			if (!__test_bit(PPPOE_FL_PADI_FAST_RETRY_BIT, &pppoe->flags))
+				retry_wait = PPPOE_DISC_TIMEOUT * (1 + s->padi_retried);
 			timer_node_add(&pppoe->session_timer , &s->t_node, retry_wait);
 			break;
 		}
@@ -375,6 +378,7 @@ pppoe_dispatch_disc_pkt(gtp_pppoe_t *pppoe, pkt_t *pkt)
 	uint32_t *hunique;
 	uint8_t code = 0;
 	uint8_t tmp[PPPOE_BUFSIZE];
+	int retry_wait = 2;
 
 	ret = pppoe_sanitize_pkt(pppoe, pkt, &off, &session, &plen, &code);
 	if (ret < 0)
@@ -517,8 +521,9 @@ breakbreak:
 			PPPDEBUG(("%s: pppoe hunique:0x%.8x failed to send PADR (%m)\n",
 				 pppoe->ifname, s->unique));
 		}
-		timer_node_add(&pppoe->session_timer, &s->t_node
-						    , PPPOE_DISC_TIMEOUT * (1 + s->padr_retried));
+		if (!__test_bit(PPPOE_FL_PADI_FAST_RETRY_BIT, &pppoe->flags))
+			retry_wait = PPPOE_DISC_TIMEOUT * (1 + s->padr_retried);
+		timer_node_add(&pppoe->session_timer, &s->t_node, retry_wait);
 		break;
 	case PPPOE_CODE_PADS:
 		if (s == NULL)

commit c54e605c5658c42f245ca02d6d7a796b6d6137af

timer: TBC __timer_node_del() compilation
Since there is no return, let's "void" it.

Fixes: 9fbaf548980 ("timer: fix timer_node_add")

lib/timer.c
@@ -259,7 +259,7 @@ timer_node_pending(timer_node_t *t_node)
 	return timerisset(&t_node->sands);
 }
 
-static int
+static void
 __timer_node_del(timer_thread_t *t, timer_node_t *t_node)
 {
 	rb_erase_cached(&t_node->n, &t->timer);