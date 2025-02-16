91729c7589258a7bed328845307c1af3fa44c001
Vincent Jardin <vjardin@free.fr>
2025-02-16 17:58:44 +0000

    fix PPPOE SIGTERM
    The daemon does not quit in case of a SIGTERM when a PPPoE session is
    established but there is no PPPOE signaling traffic.
    
    We can notice that it keeps blocking on recvmmsg() syscall:
    [pid 262269] recvmmsg(46,  <unfinished ...>
    [pid 262268] recvmmsg(44,  <unfinished ...>
    [pid 262264] recvmmsg(43,  <unfinished ...>
    
    Those fd are:
    gtp-guard 262250 root   43u     pack            4610713      0t0 PPP_SES type=SOCK_RAW
    gtp-guard 262250 root   44u     pack            4641817      0t0 PPP_DIS type=SOCK_RAW
    gtp-guard 262250 root   46u     pack            4629533      0t0 PPP_SES type=SOCK_RAW

--- src/gtp_pppoe.c
+++ src/gtp_pppoe.c
@@ -353,6 +353,11 @@ gtp_pppoe_worker_task(void *arg)
 	}
 
 	signal_noignore_sig(SIGUSR1);
+
+	/* Set Cancellation before a blocking syscall such as recvmmsg() */
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
 	log_message(LOG_INFO, "%s(): Starting PPPoE Worker %s"
 			    , __FUNCTION__, pname);
 
@@ -419,6 +424,8 @@ gtp_pppoe_worker_release(gtp_pppoe_worker_t *w)
 		return;
 	
 	pthread_kill(w->task, SIGUSR1);
+	sched_yield(); /* yield to handle the SIGUSR1 */
+	pthread_cancel(w->task); /* stop all the blocking syscalls, recvmmsg() */
 	pthread_join(w->task, NULL);
 	mpkt_destroy(&w->mpkt);
 	pkt_queue_destroy(&w->pkt_q);
